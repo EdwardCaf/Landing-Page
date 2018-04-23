32b84e39626ff19855cff1de74860de0c6efc030
Edward <Ed_caf@protonmail.com>
2018-04-23 14:52:04 -0400

    April 23rd revamping About section -- Before

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(this).scrollTop(0);
 });
-$(document).ready(function() {
+$(document).ready(function () {
     /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
+    $(window).scroll(function () {
         /* Check the location of each desired element */
-        $('.animheader').each( function(i){
+        $('.animheader').each(function (i) {
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},800);
+            if (bottom_of_window > bottom_of_object) {
+                $(this).animate({ 'opacity': '1' }, 800);
             }
         });
     });
 });
-$(document).ready(function() {
+$(document).ready(function () {
     /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
+    $(window).scroll(function () {
         /* Check the location of each desired element */
-        $('.animcontent').each( function(i){
+        $('.animcontent').each(function (i) {
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+            if (bottom_of_window > bottom_of_object) {
                 $(this).removeClass("animcontent");
                 $(this).addClass("animated fadeInDown")
             }
         });
     });
 });
+
